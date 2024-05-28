4e28734d03c02580e0aea60f3f950e9e8d44166a
落秋枫叶 <59348827+lqfy-jhc@users.noreply.github.com>
2024-05-28 20:59:24 +0800

    添加anaconda3

---
+++ apps/anaconda3/3.0.0/scripts/setpasswd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#docker exec -it ${CONTAINER_NAME} /bin/bash -c "echo -n 'jupyter notebook password'"
+#docker exec -it ${CONTAINER_NAME} /bin/bash -c "echo"
+#docker exec -it ${CONTAINER_NAME} /bin/bash -c "echo ${JUPYTER_PASSWORD} | passwd --stdin jupyter"
+#docker exec -it ${CONTAINER_NAME} /bin/bash -c "echo"
+#docker exec -it ${CONTAINER_NAME} /bin/bash -c "echo ${JUPYTER_PASSWORD} | passwd --stdin jupyter"
+#docker exec -it ${CONTAINER_NAME} /bin/bash -c "echo"
